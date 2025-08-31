eb8599661dc73139d239c44e6499236399139174
deforu <22j1162@andrew.ac.jp>
2025-08-31 23:59:09 +0900

    refactor: デフォルトポートを8000に統一

--- main.py
+++ main.py
@@ -289,4 +289,4 @@ async def websocket_endpoint(websocket: WebSocket, mic_id: str, token: str = Dep
 
 # --- サーバーの起動 ---
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
